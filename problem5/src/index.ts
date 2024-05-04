f8333b685a9b8b78ebb993979ee1c6410bdad961
Cotchi <chien.170901@gmail.com>
2024-05-04 16:15:41 +0700

    some fixes on tsconfig and index

--- problem5/src/index.ts
+++ problem5/src/index.ts
@@ -1,25 +1,15 @@
-import * as dotenv from "dotenv";
 import express from "express";
-import cors from "cors";
-
-import { authorRouter } from "./author/author.router";
-import { bookRouter } from "./book/book.router";
-
-dotenv.config();
-
-if (!process.env.PORT) {
-  process.exit(1);
-}
-
-const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(cors());
 app.use(express.json());
-app.use("/api/authors", authorRouter);
-app.use("/api/books", bookRouter);
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" }).status(200);
+});
+
+app.listen(port, () => {
+  console.log(`Server up and running on port: ${port}`);
 });
